f63cc77066e937ae957e82223e63e115ec4e87e1
yevhenp <yevhenp@vt.edu>
2018-07-18 16:25:34 -0400

    Updated a bunch of packages from 6.0.3 to 6.0.9 and more updates for hopeful Heroku deployment

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist'));
 
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
 app.get('/*', function(req,res) {
-
-    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
