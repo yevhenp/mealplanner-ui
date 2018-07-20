61cd134ab7a0a66ed00da5e35baa06176bbc7e42
yevhenp <yevhenp@vt.edu>
2018-07-19 22:07:44 -0400

    reverting server.js

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-// //Install express server
+//Install express server
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/meal-planner'));
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname + '/dist/meal-planner/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
 // const express = require('express');
-// const path = require('path');
 // const app = express();
+// const path = require('path');
+// const port = process.env.PORT || 8000;
+// const server = require('http').Server(app);
 //
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/meal-planner'));
+// app.use(express.static(__dirname, 'dist/meal-planner'));
 //
-// // For all GET requests, send back index.html
-// // so that PathLocationStrategy can be used
-// app.get('/*', function(req,res) {
+//
+// server.listen(port, function() {
+//     console.log("App running on port " + port);
+// })
+//
+// // PathLocationStrategy
+//
+// app.get('', function(req, res) {
 //     res.sendFile(path.join(__dirname + '/dist/meal-planner/index.html'));
 // });
 //
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 8000;
-const server = require('http').Server(app);
-
-app.use(express.static(__dirname, 'dist/meal-planner'));
-
-
-server.listen(port, function() {
-    console.log("App running on port " + port);
-})
-
-// PathLocationStrategy
-
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/meal-planner/index.html'));
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/meal-planner/index.html'));
-});
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/meal-planner/index.html'));
+// });
