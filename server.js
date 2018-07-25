ae5f78f5a5d73f65bc0043d97c94b36b05221a99
yevhenp <yevhenp@vt.edu>
2018-07-24 20:14:28 -0400

    /* to * for routing

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 
-console.log('The path is ' + path);
-console.log('directory is ' + __dirname);
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -12,9 +10,7 @@ app.use(express.static(path.join(__dirname, '/dist/meal-planner')));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', (req,res) => {
-    console.log('The path is ' + path);
-    console.log('directory is ' + __dirname);
+app.get('*', (req,res) => {
     res.send(path.join(__dirname, '/dist/meal-planner/index.html'));
 });
 
