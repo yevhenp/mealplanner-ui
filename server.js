c2db6967a3299e626993b84809c79c1262d89d5b
yevhenp <yevhenp@vt.edu>
2018-07-24 20:01:24 -0400

    Attempt 7 at deploying

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 
+console.log('The path is ' + path);
+console.log('directory is ' + __dirname);
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -11,6 +13,8 @@ app.use(express.static(path.join(__dirname, '/dist/meal-planner')));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/', (req,res) => {
+    console.log('The path is ' + path);
+    console.log('directory is ' + __dirname);
     res.send(path.join(__dirname, '/dist/meal-planner/index.html'));
 });
 
